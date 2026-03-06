8004daa8ad3b33ff38a45fdeb9b0e922d54bed39
Anatoliy Nakonechnyy <anatoliy.nakonechnyy@gmail.com>
2026-03-06 22:14:21 +0200

    202603062214

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,41 @@
-import { TextField } from "@mui/material";
-import { Controller } from "react-hook-form";
+import { Box, FormLabel, TextField } from '@mui/material';
+import { Controller } from 'react-hook-form';
 
-export default function Input({ name, control, errors, label, type }: { name: string, control: any, errors: any, label: string, type?: string }) {
-  return <Controller
-    name={name}
-    control={control}
-    render={({ field }) => (
-      <TextField
-        label={label}
-        type={type || "text"}
-        id="outlined-start-adornment"
-        {...field}
-        fullWidth
-        error={!!errors[name]}
-        helperText={errors[name] ? errors[name].message : ''}
-        margin="dense"
-      />
-    )}
-  />;
+export default function Input({
+  name,
+  control,
+  errors,
+  label,
+  type,
+  defaultValue,
+}: {
+  name: string;
+  control: any;
+  errors: any;
+  label: string;
+  type?: string;
+  defaultValue?: string;
+}) {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          <FormLabel sx={{ width: 250, color: 'text.primary' }}>
+            {label}
+          </FormLabel>
+          <TextField
+            type={type || 'text'}
+            id="outlined-start-adornment"
+            {...field}
+            fullWidth
+            error={!!errors[name]}
+            helperText={errors[name] ? errors[name].message : ''}
+            margin="dense"
+          />
+        </Box>
+      )}
+    />
+  );
 }
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,12 +1,49 @@
-import { Box, Input } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 import photo from '../../asset/photo.svg';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import type { User } from '../../features/user/model/types';
+import {
+  RegistrationSchema,
+  type RegistrationFormDataType,
+} from '../../features/auth/model/types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useAppDispatch } from '../../app/hooks';
+import { createNewUser } from '../../features/user/model/userSlice';
+import Input from '../../components/Input';
+import { useAppSelector } from '../../app/hooks';
+import type { RootState } from '../../app/store';
 
 export default function Profile() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegistrationFormDataType>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    resolver: zodResolver(RegistrationSchema),
+    mode: 'onSubmit',
+  });
+
+  const dispatch = useAppDispatch();
+
+  const onFormSubmit: SubmitHandler<RegistrationFormDataType> = (data) => {
+    dispatch(createNewUser(data));
+  };
+
+  const user: User | null = useAppSelector(
+    (state: RootState) => state.user.user,
+  );
+
   return (
     <>
       <Box
@@ -48,13 +85,57 @@ export default function Profile() {
               alignItems: 'flex-start',
             }}
           >
-            <Typography>John Doe</Typography>
-            <Typography>jonsnow@example.com</Typography>
+            <Typography>{user?.name ?? ''}</Typography>
+            <Typography>{user?.email ?? ''}</Typography>
           </Box>
         </Box>
         <Divider sx={{ width: '100%' }} />
         <Typography>Personal Details</Typography>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+          <Input
+            name="name"
+            control={control}
+            errors={errors}
+            label="Name"
+            defaultValue={user?.name ?? ''}
+          />
 
+          <Input
+            name="email"
+            control={control}
+            errors={errors}
+            label="Email Address"
+            defaultValue={user?.email ?? ''}
+          />
+
+          <Input
+            name="password"
+            control={control}
+            errors={errors}
+            label="Password"
+            type="password"
+            defaultValue={user?.password ?? ''}
+          />
+
+          <Input
+            name="confirmPassword"
+            control={control}
+            errors={errors}
+            label="Confirm Password"
+            type="password"
+            defaultValue={user?.password ?? ''}
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            type="submit"
+            sx={{ mt: 3 }}
+          >
+            update
+          </Button>
+        </form>
         <Typography>Budget Settings</Typography>
         <Typography>Category Management</Typography>
         <Typography>Notifications Settings</Typography>
